e3361d68c07b8daacd12b0ae670408df36e582b1
bradleyaellis <bradleyaellis@gmail.com>
2021-04-28 09:35:29 -0500

    Styling

--- controllers/index.js
+++ controllers/index.js
@@ -10,8 +10,9 @@ exports.postRetrieveGame = (req, res, next) => {
   const data =
     axios
       .get(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.STEAM_API_KEY}&steamid=${req.body.steamid}&include_appinfo=true`)
-      .then(response => {
-        game = response.data.response.games[Math.floor(Math.random() * response.data.response.games.length)]
+      .then(resp => {
+        let games = resp.data.response.games
+        game = games[Math.floor(Math.random() * games.length)]
         res.render('index', { game: game, subtitle: "You should play. . .", steamid: req.body.steamid});
       })
       .catch(err => console.log(err));
