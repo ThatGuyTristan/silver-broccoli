30050882428f1586ce432eef4fbec388dc8a1dd8
Tristan <tristan@Brocks-MBP.home>
2024-02-05 10:58:05 -0600

    Update file, remove node_modules and package-lock

--- controllers/index.js
+++ controllers/index.js
@@ -1,26 +1,79 @@
 const axios = require("axios");
 require('dotenv').config()
 
-exports.getIndex = (req, res, next) => {
-  res.render('index', { title: 'Enter your Steam ID', game: {} })
+const DONT_PUSH_THIS_TO_GIT = 'F3EDB222C9455394555706722F3FE1BF'
+let games = [];
+let storedUsername;
+
+const resolveVanityUrl = async (username) => {
+  return new Promise((resolve) => {
+    resolve(
+      axios
+        .get(
+          `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${DONT_PUSH_THIS_TO_GIT}&vanityurl=${username}`
+        )
+      ) 
+    }
+  )
 }
 
-exports.postRetrieveGame = (req, res, next) => {
+const resolvePlaytime = (playtime) => {
+  if (playtime < 60) {
+    return "Less than one hour."
+  } else if (playtime == 0) { 
+    return "Never played"
+  } else { 
+    return Math.floor(playtime / 60 ) + " hours."
+  }
+}
+
+const resolveGameFromList = () => {
+  return games[Math.floor(Math.random() * games.length)]
+}
+
+exports.getIndex = (req, res, next) => {
+  res.render('index', { title: 'Enter your Steam ID', games: {} })
+}
+
+exports.postRetrieveGame = async (req, res, next) => {
+  let steamId;
   let game;
-  axios
-    .get(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.STEAM_API_KEY}&steamid=${req.body.steamid}&include_appinfo=true`)
-    .then(resp => {
-      let games = resp.data.response.games
-      game = games[Math.floor(Math.random() * games.length)]
-      console.log(game);
-      let played_time = 0;
-      let img_src = `http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_logo_url}.jpg`
-      if (game.playtime_forever < 60) {
-        played_time = "Less than one hour."
-      } else {
-        played_time = Math.floor(game.playtime_forever / 60 ) + " hours."
-      }
-      res.render('index', { name: game.name, img_src: img_src, played_time: played_time, steamid: req.body.steamid});
-    })
-    .catch(err => console.log(err));
+
+  await resolveVanityUrl(req.body.username).then((resp) => { 
+    steamId = resp.data.response.steamid
+  })
+
+  if(games.length === 0 || storedUsername != req.body.username) {
+    storedUsername = req.body.username
+    console.log('resolving games via steam api')
+    axios
+      .get(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${DONT_PUSH_THIS_TO_GIT}&steamid=${steamId}&include_appinfo=true`)
+      .then(resp => {
+        games = resp.data.response.games
+        game = resolveGameFromList();
+        let img_src = `http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_logo_url}.jpg`
+
+        res.render('index', { 
+          name: game.name, 
+          gamesLength: games.length, 
+          img_src: game.img_logo_url ?  img_src : '', 
+          played_time: resolvePlaytime(game.playtime_forever), 
+          username: req.body.username,
+          steamid: steamId
+        });
+
+
+      }) 
+      .catch(err => console.log(err))
+  } else { 
+    game = resolveGameFromList();
+    res.render('index', { 
+      name: game.name, 
+      gamesLength: games.length, 
+      img_src: game.img_logo_url ?  img_src : '', 
+      played_time: resolvePlaytime(game.playtime_forever), 
+      username: req.body.username,
+      steamid: steamId
+    });
+  }
 }
